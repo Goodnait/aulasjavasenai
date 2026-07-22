38231d76755462cea24305aa7a3ca00329a1fe2a
daniel <danielkira01@gmail.com>
2026-07-21 21:00:29 -0300

    incluido atividade7

---
+++ src/aulas/encapsulamento/banco/modelo/ContaBancaria.java
@@ -0,0 +1,51 @@
+package aulas.encapsulamento.banco.modelo;
+
+public class ContaBancaria {
+
+    private final String titular;
+    private int numeroDaConta;
+    private double saldo;
+
+    private static int contadorContas = 1000;
+
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        contadorContas++;
+        this.numeroDaConta = contadorContas;
+        this.saldo = 0.0;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println(valor + " foi depositado");
+        } else {
+            System.out.println("Erro: valor inválido");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && this.saldo >= valor) {
+            this.saldo -= valor;
+            System.out.println(valor + " foi retirado da conta");
+        } else {
+            System.out.println("Erro: saldo insuficiente ou valor inválido");
+        }
+    }
+
+    private String acessarTodosOsDados() {
+        return this.titular;
+    }
+}
